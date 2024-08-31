7bffb57e35751960ed1d3c012140b34d8c6dae8a
Ajit <100071266+Ajitashoka@users.noreply.github.com>
2024-08-31 12:50:49 +0530

    Add files via upload

---
+++ apieg1.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+# from llama_cpp import Llama
+from llama_index.embeddings.bedrock import BedrockEmbedding
+from llama_index.core import Settings
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.llms.bedrock import Bedrock
+import json
+import filetype, os
+
+llm = Bedrock(
+    model="meta.llama3-8b-instruct-v1:0",
+    aws_access_key_id=os.getenv("AWS_ID"),
+    aws_secret_access_key=os.getenv("AWS_KEY"),
+    region_name="us-east-1"
+)
+app = FastAPI()
+
+origins =["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class InferenceRequest(BaseModel):
+    prompt: str
+
+@app.post("/complete")
+def perform_inference(request: InferenceRequest):
+    output = llm(
+        request.prompt,
+        max_tokens=48,
+        stop=["Q:", "\n"],
+        echo=True,
+    )
+    return {"data": output}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
