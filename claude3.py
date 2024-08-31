8465970e4fcf4f382010ef3254ee3f47cf6712ec
Ajit <100071266+Ajitashoka@users.noreply.github.com>
2024-08-31 12:55:13 +0530

    Add files via upload

---
+++ claude3.py
@@ -0,0 +1,36 @@
+# -----------------------------------------------
+# Setting up Amazon Bedrock with AWS SDK Boto3 🐍️
+# -----------------------------------------------
+
+import boto3, json, os
+bedrock_runtime=boto3.client(service_name="bedrock-runtime",
+                             aws_access_key_id=os.getenv("AWS_ID"),
+    aws_secret_access_key=os.getenv("AWS_KEY"),
+                             region_name="us-east-1",
+                             )
+# Model configuration 
+model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+model_kwargs =  {
+    "max_tokens": 2048, "temperature": 0.1,
+    "top_k": 250, "top_p": 1, "stop_sequences": ["\n\nHuman"],
+}
+# Input configuration
+
+prompt = "Act as a Shakespeare and write a poem on Genertaive AI"
+body = {
+    "anthropic_version": "bedrock-2023-05-31",
+    "system": "You are a honest and helpful bot.",
+    "messages": [
+        {"role": "user", "content": [{"type": "text", "text": prompt}]},
+    ],
+}
+body.update(model_kwargs)
+# Invoke
+response = bedrock_runtime.invoke_model(
+    modelId=model_id,
+    body=json.dumps(body),
+)
+# Process and print the response
+
+result = json.loads(response.get("body").read()).get("content", [])[0].get("text", "")
+print(result)
